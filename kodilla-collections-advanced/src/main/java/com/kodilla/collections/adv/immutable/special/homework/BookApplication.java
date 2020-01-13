25b372ae6798f3a1965e717b952b2cc5354d2cf1
Irmina Gajek <irmina.gajek@gmail.com>
2020-01-13 11:18:51 +0100

    Module 5 Homework Book

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
+import static com.kodilla.collections.adv.immutable.special.homework.BookManager.books;
+
 public class BookApplication {
 
     public static void main(String[] args) {
@@ -9,9 +11,9 @@ public class BookApplication {
         Book book3 = BookManager.createBook("Knife", "Jo Nesbo");
         Book book4 = BookManager.createBook("Knife", "Jo Nesbo");
 
-        System.out.println(book1);
-        System.out.println(book2);
-        System.out.println(book3);
+        System.out.println(books.size());
+        for (Book book : books)
+            System.out.println(book);
 
         System.out.println(book1 == book2);
         System.out.println(book1 == book3);
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,36 +1,15 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BookManager {
 
-    String author;
-    String title;
+    static Set<Book> books = new HashSet<>();
 
-    static  List<Book> books = new ArrayList<>();
     public static Book createBook(String title, String author) {
         Book book = new Book(title, author);
         books.add(book);
         return book;
-    }
-
-    public static List<Book> showBooksList() {
-        return books;
-    }
-
-    @Override
-    public boolean equals(Object book) {
-        if (this == book) return true;
-        if (book == null || getClass() != book.getClass()) return false;
-        BookManager that = (BookManager) book;
-        return Objects.equals(author, that.author) &&
-                Objects.equals(title, that.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(author, title);
     }
 }
