88fe83fe4414099b3aa3a49b727d8281fd1f0481
Irmina Gajek <irmina.gajek@gmail.com>
2020-01-16 19:47:07 +0100

    Module 6 Homework Task

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -8,10 +8,10 @@ public class TaskRepository {
 
     public static List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("zadanie1", LocalDate.of(2019, 12, 7), LocalDate.of(2020, 1, 10)));
-        tasks.add(new Task("zadanie2", LocalDate.of(2019, 12, 15), LocalDate.of(2020, 1, 15)));
-        tasks.add(new Task("zadanie3", LocalDate.of(2020, 1, 10), LocalDate.of(2020, 3, 31)));
-        tasks.add(new Task("zadanie3", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 2, 28)));
+        tasks.add(new Task("Task1", LocalDate.of(2019, 12, 7), LocalDate.of(2020, 1, 10)));
+        tasks.add(new Task("Task2", LocalDate.of(2019, 12, 15), LocalDate.of(2020, 1, 15)));
+        tasks.add(new Task("Task3", LocalDate.of(2020, 1, 10), LocalDate.of(2020, 3, 31)));
+        tasks.add(new Task("Task4", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 2, 28)));
 
         return tasks;
     }
