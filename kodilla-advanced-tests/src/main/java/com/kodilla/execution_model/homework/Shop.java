24135f6a6e4d2adafec212d56e589b263434fe56
Irmina Gajek <irmina.gajek@gmail.com>
2020-01-28 16:02:29 +0100

    Module 7 Homework Shop

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,51 +1,38 @@
 package com.kodilla.execution_model.homework;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Shop {
 
     private List<Order> orders = new ArrayList<>();
 
     public boolean addOrder(Order order) {
+        if (order.value <= 0 || order.dateOfIssue == null || order.login == null) {
+            return false;
+        }
         return this.orders.add(order);
     }
 
-    public List<LocalDate> returnOrdersBasedOnDates(LocalDate dateA, LocalDate dateB) throws OrderNotFoundException {
-        List<Order> foundOrders = new ArrayList<>();
-        long numbersBetween = ChronoUnit.DAYS.between(dateA, dateB);
-        return IntStream.iterate(0, i -> i + 1)
-                .limit(numbersBetween)
-                .mapToObj(i -> dateA.plusDays(i))
-                .collect(Collectors.toList());
-
-//        for (int n = 0; n < orders.size(); n++) {
-//            Order order = orders.get(n);
-//            if (order.getDateOfIssue().isAfter(LocalDate.of (dateA) && order.getDateOfIssue().isBefore(dateB))) {
-//                foundOrders.add(order);
-//        }
-//        if (foundOrders.size() > 0) {
-//            return foundOrders;
-//        } else
-//            throw new OrderNotFoundException();
-//    }
+    public List<Order> returnOrdersBasedOnDates(LocalDate dateA, LocalDate dateB) {
+        if(this.orders.size() > 0){
+            return this.orders.stream()
+                    .filter(t -> t.getDateOfIssue().isAfter(dateA) && t.getDateOfIssue().isBefore(dateB))
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 
-    public List<Order> getOrderBasedOnValues(double valueMin, double valueMax) throws OrderNotFoundException {
-            List<Order> foundValues = new ArrayList<>();
-        for (int n = 0; n < orders.size(); n++) {
-            Order order = orders.get(n);
-            if (order.getValue() > valueMin && order.getValue() < valueMax)
-                foundValues.add(order);
+    public List<Order> getOrderBasedOnValues(double valueMin, double valueMax) {
+        if (this.orders.size() > 0) {
+            return this.orders.stream()
+                    .filter(v -> v.getValue() > valueMin && v.getValue() < valueMax)
+                    .collect(Collectors.toList());
         }
-            if (foundValues.size() > 0) {
-                return foundValues;
-            } else
-        throw new OrderNotFoundException();
+        return Collections.emptyList();
     }
 
     public int getSize() {
