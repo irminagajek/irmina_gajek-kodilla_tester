ca670362ff1d9160b5eb5a58cfe017e7aa9cbaa3
Irmina Gajek <irmina.gajek@gmail.com>
2020-02-19 21:50:20 +0100

    Module 10 Homework BookController

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
@@ -8,28 +8,42 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BookControllerMvcTestSuite {
 
     @Test
     public void shouldAddBook() {
 
+        //given
         BookService bookServiceMock = Mockito.mock(BookService.class);
         BookController bookController = new BookController(bookServiceMock);
         List<BookDto> myBooks = new ArrayList<>();
-        BookDto book1 = new BookDto("a", "b");
-        BookDto book2 = new BookDto("c", "d");
-        BookDto book3 = new BookDto("e", "f");
-        BookDto book4 = new BookDto("e", "f");
-        myBooks.add(book1);
-        myBooks.add(book2);
-        myBooks.add(book3);
+        myBooks.add(new BookDto("Nóż", "Jo Nesbo"));
+        myBooks.add(new BookDto("Policja", "Jo Nesbo"));
+        myBooks.add(new BookDto("Pierwszy śnieg", "Jo Nesbo"));
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(myBooks);
 
         //when
+        List<BookDto> result = bookController.getBooks();
 
-        Mockito.when(bookServiceMock.getBooks().add(book4)).thenReturn(myBooks.size());
+        //then
+        assertEquals(result.size(), myBooks.size());
+        assertTrue(result.size() == 3);
+    }
 
-        assertNotEquals(0, myBooks.size());
+    @Test
+    public void isBookAdded() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> myBooks = new ArrayList<>();
+        BookDto book1 = new BookDto("Nóż", "Jo Nesbo");
+        myBooks.add(book1);
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(myBooks);
+
+        List<BookDto> result = bookController.getBooks();
+
+        assertEquals(result.size(), myBooks.size());
     }
 }
