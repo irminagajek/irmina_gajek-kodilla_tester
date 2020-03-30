c7582e840e86d7e41725c4498de0c23718dbb5e3
Irmina Gajek <irmina.gajek@gmail.com>
2020-03-30 18:59:26 +0200

    Module 7 Homework AlertService

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/AlertService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/AlertService.java
@@ -1,34 +1,51 @@
 package com.kodilla.mockito.homework;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AlertService {
 
     Map<Location, List<Person>> locationAssignments = new HashMap<>();
+    private List<Person> people = new ArrayList<>();
 
     public void subscribe(Person person, Location location) {
+        locationAssignments.putIfAbsent(location, new ArrayList<>());
+        List<Person> list = locationAssignments.get(location);
+        list.add(person);
+    }
 
+    public void addSubscriber(Person person) {
+        this.people.add(person);
+    }
+
+    public void sendAlert(Alert alert) {
+        this.people.forEach(person -> person.receive("Uwaga!"));
     }
 
     public void unsubscribe(Person person, Location location) {
-
+        if (locationAssignments.containsKey(location)) {
+            List<Person> list = locationAssignments.get(location);
+            list.remove(person);
+        }
     }
 
-    public void unsubscribe(Person person) {
+    public void totalUnsubscribe(Person person) {
+        locationAssignments.forEach((location, plist) -> {
+         plist.remove(person);
+    });
 
+        locationAssignments.values().forEach(plist -> {
+            plist.remove(person);
+        });
     }
 
-    public void alert(Location location) {
-
-    }
-
-    public void alertAll() {
-
-    }
+    public void sendLocalAlert(Location location) {
+            if (locationAssignments.containsKey(location)) {
+                List<Person> list = locationAssignments.get(location);
+                list.forEach(p -> p.receive("Uwaga miasto!"));
+            }
+        }
 
     public void remove(Location location){
-
+    locationAssignments.remove(location);
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/AlertServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/AlertServiceTestSuite.java
@@ -15,4 +15,66 @@ public class AlertServiceTestSuite {
         alertService.alert(Location.WARSAW);
         Mockito.verify(person, Mockito.times(1)).receive(location); //
     }
+//
+//    @Test
+//    public void shouldAddPersonToLocalAlert() {
+//        AlertService alertService = new AlertService();
+//        Person person = Mockito.mock(Person.class);
+//        alertService.subscribe(person, Location.CRACOW);
+//        Mockito.verify(person, )
+//    //osoba zapisana dostaje 1 raz alert zwiazany z lokalizacja
+//    }
+
+    @Test
+    public void shouldDeleteSubscriptionFromLocalAlert() {
+        AlertService alertService = new AlertService();
+        Person person = Mockito.mock(Person.class);
+        alertService.subscribe(person, Location.CRACOW);
+        Alert alert = Mockito.mock(Alert.class);
+        alertService.unsubscribe(person, Location.CRACOW);
+
+        alertService.sendAlert(alert);
+        Mockito.verify(person, Mockito.never()).receive("Uwaga!");
+    }
+
+    @Test
+    public void shouldDeleteSubscriptionFromAllLocations() {
+    AlertService alertService = new AlertService();
+    Person person = Mockito.mock(Person.class);
+    alertService.subscribe(person, Location.CRACOW);
+    alertService.subscribe(person, Location.WARSAW);
+    Alert alert = Mockito.mock(Alert.class);
+    alertService.totalUnsubscribe(person);
+
+    alertService.sendAlert(alert);
+    Mockito.verify(person, Mockito.never()).receive("Uwaga!");
+    }
+
+    @Test
+    public void shouldSentAlertsToAllSubscribedPeople() {
+        AlertService alertService = new AlertService();
+        Person firstPerson = Mockito.mock(Person.class);
+        Person secondPerson = Mockito.mock(Person.class);
+        Person thirdPerson = Mockito.mock(Person.class);
+        alertService.addSubscriber(firstPerson);
+        alertService.addSubscriber(secondPerson);
+        alertService.addSubscriber(thirdPerson);
+        Alert alert = Mockito.mock(Alert.class);
+
+        alertService.sendAlert(alert);
+        Mockito.verify(firstPerson, Mockito.times(1)).receive("Uwaga!");
+        Mockito.verify(secondPerson, Mockito.times(1)).receive("Uwaga!");
+        Mockito.verify(thirdPerson, Mockito.times(1)).receive("Uwaga!");
+    }
+
+    @Test
+    public void shouldDeleteLocation() {
+    AlertService alertService = new AlertService();
+    Alert alert = Mockito.mock(Alert.class);
+
+    alertService.remove(Location.CRACOW);
+    alertService.remove(Location.WARSAW);
+
+    //osoba nie może się dopisać do lokalizacji, która nie istnieje
+    }
 }
